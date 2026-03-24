63767cf1eaaeba161103d5121b5d364f65350f9d
vld.smelov <vldsmelov@yandex.ru>
2026-03-24 17:07:01 +0300

    feat(ui): add footer branding text

--- ui/components/Layout.tsx
+++ ui/components/Layout.tsx
@@ -1,7 +1,10 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="lg:pl-20 bg-light-primary dark:bg-dark-primary min-h-screen">
-      <div className="max-w-screen-lg lg:mx-auto mx-4">{children}</div>
+    <main className="lg:pl-20 bg-light-primary dark:bg-dark-primary min-h-screen flex flex-col">
+      <div className="max-w-screen-lg lg:mx-auto mx-4 flex-1">{children}</div>
+      <footer className="max-w-screen-lg lg:mx-auto mx-4 pb-24 lg:pb-6 pt-4 text-center text-xs text-black/60 dark:text-white/60">
+        {'by Модуль "Цифровизация проектных задач"'}
+      </footer>
     </main>
   );
 };
